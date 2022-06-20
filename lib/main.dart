5de4c5f90bcd52be21681fbcc435f640b8aca623
Valk Dokk <thevalkdokk@gmail.com>
2022-06-20 16:08:56 +0700

    change Text

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Material App',
+      title: 'Material Apps',
       home: Welcomescreen(),
     );
   }
